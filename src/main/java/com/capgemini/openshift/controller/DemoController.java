e5072580e9d1b20b6280a164b2b4011433ff4acd
AnshuSoni <meabhisheksoni@gmail.com>
2022-01-03 16:14:39 +0530

    Docker file

--- src/main/java/com/capgemini/openshift/controller/DemoController.java
+++ src/main/java/com/capgemini/openshift/controller/DemoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.capgemini.openshift.models.Employee;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/api/v1")
 public class DemoController {
 
